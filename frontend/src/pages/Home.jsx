e323bc7e97536be9d6fb9f927def73a2a4a09ee0
k3xxv1n <kevincalisto001@gmail.com>
2025-09-07 20:54:48 -0300

    Optimized material ui in home and New Design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar"
 
-import Button from "@mui/material/Button"
+import {Button, IconButton, Box} from "@mui/material"
+import GitHubIcon from '../assets/github.png'
 
 import { Link} from "react-router-dom"
 
@@ -11,6 +12,11 @@ const Home = () => {
             <div className="relative top-35">
                 <h1 className="text-center text-4xl">Hi, I'm k3xxv1n<br/>Software developer</h1>
                 <p className="text-center">My name is k3xxv1n (Kevin), this is my portafolio web</p>
+                <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                    <IconButton  href="https://github.com/k3xxv1n" target="_blank">
+                        <img src={GitHubIcon} alt="GitHub" className="w-10 h-10 mx-auto mt-4" />
+                    </IconButton>
+                </Box>              
                 <div className="flex justify-center gap-4 relative top-5">
                     <Button component={Link} variant="outlined" sx={
                         {
@@ -26,6 +32,7 @@ const Home = () => {
                     }} to='/About'>About me</Button>
                 </div>
             </div>
+
         </div>
     )
 }
