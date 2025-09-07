177b10a3cd3f3dae17b66848d0cacfc7c2d07bbc
k3xxv1n <kevincalisto001@gmail.com>
2025-09-07 19:53:53 -0300

    Add SEO to Web (1.0)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
         <div className="">
             <Navbar />
             <div className="relative top-35">
-                <h1 className="text-center text-4xl">Welcome to me portafolio</h1>
-                <p className="text-center">My name is kevin, this is my portafolio web</p>
+                <h1 className="text-center text-4xl">Welcome to me portfolio</h1>
+                <p className="text-center">My name is k3xxv1n (Kevin), this is my portafolio web</p>
                 <div className="flex justify-center gap-4 relative top-5">
                     <Button component={Link} variant="outlined" sx={
                         {
