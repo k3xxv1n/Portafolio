76e9e8b98ac4d4db59e0bdaa63f9d395df77c43d
k3xxv1n <kevincalisto001@gmail.com>
2025-10-03 22:48:29 -0300

    se modifico el portafoli completo para que se vea mas minimalista

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Navbar from "../components/Navbar"
 
 import {Button, IconButton, Box} from "@mui/material"
 import GitHubIcon from '../assets/github.png'
+import IgIcon from '../assets/instagram.png'
+import DcIcon from '../assets/discord.png'
 
 import { Link} from "react-router-dom"
 
@@ -12,11 +14,19 @@ const Home = () => {
             <div className="relative top-35">
                 <h1 className="text-center text-4xl">Hi, I'm k3xxv1n<br/>Software developer</h1>
                 <p className="text-center">My name is k3xxv1n (Kevin), this is my portafolio web</p>
+
                 <Box sx={{ display: 'flex', justifyContent: 'center'}}>
                     <IconButton  href="https://github.com/k3xxv1n" target="_blank">
                         <img src={GitHubIcon} alt="GitHub" className="w-10 h-10 mx-auto mt-4" />
                     </IconButton>
-                </Box>              
+                    <IconButton  href="https://www.instagram.com/k3nvhz.k3" target="_blank">
+                        <img src={IgIcon} alt="instagram" className="w-10 h-10 mx-auto mt-4" />
+                    </IconButton>
+                    <IconButton href="https://discord.com/users/123456789012345678" target="_blank">
+                        <img src={DcIcon} alt="Discord" className="w-11 h-11 mx-auto mt-4" />
+                    </IconButton>
+                </Box>
+
                 <div className="flex justify-center gap-4 relative top-5">
                     <Button component={Link} variant="contained" sx={
                         {
